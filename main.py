41067bafb8eae86eda72b0506e8f3b74cd265a3d
henrythier <henry@thierhoff.rocks>
2020-06-29 18:26:54 +0200

    minor changes

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ outlets = [faz, tonline, zeit]
 counter = 0
 
 def get_openers():
+    now = datetime.datetime.now()
+    print('RUN AT: {0}'.format(now))
     for o in outlets:
         try:
             opener = vars(o.get_opener())
@@ -18,7 +20,7 @@ def get_openers():
         except:
             e = sys.exc_info()
             print('Error: {0},\nAt: {1},\nOutlet: {2}'.format(e[0],
-                                                              datetime.datetime.now(),
+                                                              now,
                                                               o.name))
 
 schedule.every().hour.do(get_openers)
